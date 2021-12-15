69a64bde1a3df9d157ad14b164866d33cf396296
Pedro Drago <dragope@hotmail.com>
2021-12-14 21:27:50 -0300

    Routing with Loading effect

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,50 +1,67 @@
 import React from 'react'
 import './ItemDetail.css'
 import ItemCount from './ItemCount'
+import { useState, useEffect } from 'react'
 import visa from './images/tarjetas/visa.png'
 import amex from './images/tarjetas/amex.png'
 import master from './images/tarjetas/mastercard.png'
 
 function ItemDetail(props){
 
+    const [load, setLoad] = useState(true)
+
+    useEffect(()=>{
+        const waitLoader = new Promise((resolve)=>{
+            setTimeout(()=>{
+                resolve(setLoad(!load))
+            }, 2000)
+        })
+    }, [])
+
     const handleAddQuantity =(param)=>{
         alert("You added " + param + " product/s to your shopping cart")
     }
 
     return(
-        <div className="detalles_producto">
-            <div className="detalles_producto__fotos">
-                <img alt="Guitarra" src={ props.pic1 } className="foto_miniatura"/>
-                <img alt="Guitarra" src={ props.pic2 } className="foto_miniatura"/>
-                <img alt="Guitarra" src={ props.pic3 } className="foto_grande"/>
-            </div>
-            <div className="detalles_producto__detalles">
-                <h1 className="detalles_producto__titulo">{ props.title }</h1>
-                <h1 className="detalles_producto__precio">${ props.price }.00</h1>
-                <div className="detalles_producto__cuotas">
-                    <img alt="Tarjeta de Credito" src={ visa }/>
-                    <img alt="Tarjeta de Credito" src={ master }/>
-                    <img alt="Tarjeta de Credito" src={ amex }/>
-                    <h5>ALL ACCEPTED</h5>
-                    <h4>{ props.description }</h4>
+        <div>
+            { load ? 
+                <div className="loader"></div>
+                : 
+                <div className="detalles_producto">
+                    <div className="detalles_producto__fotos">
+                        <img alt="Guitarra" src={ props.pic1 } className="foto_miniatura"/>
+                        <img alt="Guitarra" src={ props.pic2 } className="foto_miniatura"/>
+                        <img alt="Guitarra" src={ props.pic3 } className="foto_grande"/>
+                    </div>
+                    <div className="detalles_producto__detalles">
+                        <h1 className="detalles_producto__titulo">{ props.title }</h1>
+                        <h1 className="detalles_producto__precio">${ props.price }.00</h1>
+                        <div className="detalles_producto__cuotas">
+                            <img alt="Tarjeta de Credito" src={ visa }/>
+                            <img alt="Tarjeta de Credito" src={ master }/>
+                            <img alt="Tarjeta de Credito" src={ amex }/>
+                            <h5>ALL ACCEPTED</h5>
+                            <h4>{ props.description }</h4>
+                        </div>
+                        <hr/>
+                        <div className="detalles_producto__colores">
+                            <h4>COLORS</h4>
+                            <button>Red</button>
+                            <button>Black</button>
+                            <button>Blue</button>
+                            <button>Yellow</button>
+                            <button>Green</button>
+                            <button>Sunburst</button>
+                        </div>
+                        <div className="detalles_producto__final">
+                            <input type="text" placeholder="Zip Code"/>
+                            <button>CALCULATE SHIPPING COST</button>
+                            <p><a href="https://tools.usps.com/zip-code-lookup.htm">Check you Zip Code</a></p>
+                        </div>
+                        <ItemCount stock={ props.stock } initial={ 1 } onAdd={ handleAddQuantity }/>
+                    </div>
                 </div>
-                 <hr/>
-                <div className="detalles_producto__colores">
-                    <h4>COLORS</h4>
-                    <button>Red</button>
-                    <button>Black</button>
-                    <button>Blue</button>
-                    <button>Yellow</button>
-                    <button>Green</button>
-                    <button>Sunburst</button>
-                </div>
-                <div className="detalles_producto__final">
-                    <input type="text" placeholder="Zip Code"/>
-                    <button>CALCULATE SHIPPING COST</button>
-                    <p><a href="https://tools.usps.com/zip-code-lookup.htm">Check you Zip Code</a></p>
-                </div>
-                <ItemCount stock={ props.stock } initial={ 1 } onAdd={ handleAddQuantity }/>
-            </div>
+            }
         </div>
         
     )
