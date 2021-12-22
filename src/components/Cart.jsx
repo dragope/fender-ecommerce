7dcc405ae449fedfcffa3b4dfb0d68279af81e68
dragope <dragope@hotmail.com>
2021-12-22 16:25:22 -0300

    Cart Functionalities

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import './Cart.css'
+import { Link } from "react-router-dom";
 import { useState } from 'react'
 import {useCartContext} from '../context/CartContext'
 
@@ -16,29 +17,53 @@ function Cart(){
     return(
         <center>
             { load ?
-                <div className="loader"></div>
-                :
-                <div className="cartpage-container">
-                    { cartList.map(prod => 
+    <div className="loader"></div>
+    :
+    cartList[0] === undefined ?
+    <div className="cartpage-container">
+        <h3>Your shopping cart is currently empty</h3>
+        <Link to="/"><button className="start-shopping">Start shopping here!</button></Link>
+    </div>
+    :
+    <div className="cartpage-container">
+        { cartList.map(prod => 
 
-                    <div className="cart-product-container">
-                        <img className="cart-product-image" alt="Guitar" src={prod.pictureDetail1}/>
-                        <p className="cart-product-title">{prod.title}</p>
-                        <p className="cart-product-quantity">{prod.quantity}</p>
-                        <p className="cart-product-price">${prod.price}.00</p>
-                    </div>
-                    
-
-
-                    )}
-                    <button onClick={emptyCart}>Empty Shopping Cart</button>
-                </div>
-                
-            }
+        <div className="cart-product-container">
+            <img className="cart-product-image" alt="Guitar" src={prod.pictureDetail1}/>
+            <p className="cart-product-title">{prod.title}</p>
+            <p className="cart-product-quantity">{prod.quantity}</p>
+            <p className="cart-product-price">${prod.price}.00</p>
+        </div>
+        )}
+        <button onClick={emptyCart}>Empty Shopping Cart</button>
+    </div>
+    
+}
         </center>
     )
 }
 
 export default Cart
 
-{/* <li key={prod.id}>{prod.title} {prod.quantity} {prod.price}</li> */}
+
+
+
+
+
+// { load ?
+//     <div className="loader"></div>
+//     :
+//     <div className="cartpage-container">
+//         { cartList.map(prod => 
+
+//         <div className="cart-product-container">
+//             <img className="cart-product-image" alt="Guitar" src={prod.pictureDetail1}/>
+//             <p className="cart-product-title">{prod.title}</p>
+//             <p className="cart-product-quantity">{prod.quantity}</p>
+//             <p className="cart-product-price">${prod.price}.00</p>
+//         </div>
+//         )}
+//         <button onClick={emptyCart}>Empty Shopping Cart</button>
+//     </div>
+    
+// }
